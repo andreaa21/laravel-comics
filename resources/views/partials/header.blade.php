a2c81188900941e7aa68335fcd6324b64e397447
andreaa21 <andrea.zollini@proton.me>
2022-12-22 16:44:20 +0100

    add class active to header nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,16 +6,26 @@
 
         <nav>
             <ul>
-                <li><a href="{{ route('characters') }}">Characters</a></li>
-                <li><a href="{{ route('comics') }}">Comics</a></li>
-                <li><a href="{{ route('movies') }}">Movies</a></li>
-                <li><a href="{{ route('tv') }}">tv</a></li>
-                <li><a href="{{ route('games') }}">Games</a></li>
-                <li><a href="{{ route('collectibles') }}">Collectibles</a></li>
-                <li><a href="{{ route('videos') }}">Videos</a></li>
-                <li><a href="{{ route('fans') }}">Fans</a></li>
-                <li><a href="{{ route('news') }}">News</a></li>
-                <li><a href="{{ route('shop') }}">Shop</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'characters' ? 'active' : '' }}"
+                        href="{{ route('characters') }}">Characters</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'comics' ? 'active' : '' }}"
+                        href="{{ route('comics') }}">Comics</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'movies' ? 'active' : '' }}"
+                        href="{{ route('movies') }}">Movies</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'tv' ? 'active' : '' }}"
+                        href="{{ route('tv') }}">tv</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'games' ? 'active' : '' }}"
+                        href="{{ route('games') }}">Games</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'collectibles' ? 'active' : '' }}"
+                        href="{{ route('collectibles') }}">Collectibles</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'videos' ? 'active' : '' }}"
+                        href="{{ route('videos') }}">Videos</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'fans' ? 'active' : '' }}"
+                        href="{{ route('fans') }}">Fans</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'news' ? 'active' : '' }}"
+                        href="{{ route('news') }}">News</a></li>
+                <li><a class="{{ Route::currentRouteName() === 'shop' ? 'active' : '' }}"
+                        href="{{ route('shop') }}">Shop</a></li>
             </ul>
         </nav>
 
