119a7c8e585bea4fd791c4c7889e10a4c249d9a2
andreaa21 <andrea.zollini@proton.me>
2022-12-23 15:22:28 +0100

    add cards

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -7,5 +7,19 @@
         </div>
     </div>
 
-    @dump($comics)
+    <main>
+        <div class="container cards">
+            @foreach ($comics as $comic)
+                <div class="x-card">
+                    <div class="card-img">
+                        <img src="{{ $comic['thumb'] }}" :alt="{{ $comic['title'] }}">
+                    </div>
+                    <div class="card-text">{{ $comic['series'] }}</div>
+                </div>
+            @endforeach
+        </div>
+        <div class="btn-container d-flex justify-content-center">
+            <button>load more</button>
+        </div>
+    </main>
 @endsection
